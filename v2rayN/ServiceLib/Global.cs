9361bd1385a29b7005e5cbd0a9454f2738b7849c
tristesky <52591934+tristesky@users.noreply.github.com>
2026-03-31 14:02:58 +0800

    推广

--- v2rayN/ServiceLib/Global.cs
+++ v2rayN/ServiceLib/Global.cs
@@ -10,7 +10,7 @@ public class Global
     public const string GeoUrl = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/{0}.dat";
     public const string SingboxRulesetUrl = @"https://raw.githubusercontent.com/2dust/sing-box-rules/rule-set-{0}/{1}.srs";
 
-    public const string PromotionUrl = @"aHR0cHM6Ly85LjIzNDQ1Ni54eXovYWJjLmh0bWw=";
+    public const string PromotionUrl = @"aHR0cHM6Ly9lbi5oaWZ1OC5jb20=";
     public const string ConfigFileName = "guiNConfig.json";
     public const string CoreConfigFileName = "config.json";
     public const string CorePreConfigFileName = "configPre.json";
